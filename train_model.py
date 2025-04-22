8f2d76eba394601ac8a91638acb048606a6e5d00
Abhishek Angadi <153805526+Shadow3456rh@users.noreply.github.com>
2025-04-22 23:03:15 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -62,7 +62,7 @@ for filename in os.listdir(DATA_DIR):
         X_scaled = scaler.fit_transform(X)
 
         # Train-test split
-        X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.3, random_state=42)
 
         model = LinearRegression()
         model.fit(X_train, y_train)
