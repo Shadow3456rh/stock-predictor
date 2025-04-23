cd11b1e210a19e9ca407d14e7f18bbb9fef128c2
Abhishek Angadi <153805526+Shadow3456rh@users.noreply.github.com>
2025-04-23 11:34:41 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -20,7 +20,7 @@ s3 = boto3.client('s3',
                   aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
                   aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
 
-print("Fetching stock data from S3...")
+print("Fetching stock data from S3")
 
 response = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=PREFIX)
 stock_files = [obj['Key'] for obj in response.get('Contents', []) if obj['Key'].endswith('_data.csv')]
